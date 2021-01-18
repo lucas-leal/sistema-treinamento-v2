f3bbc4854f2bb450d5886f977ddfec11b866f0e8
lucasleal <lucas.ity.leal@gmail.com>
2021-01-18 19:33:45 -0300

    Creating more tables in database

--- database/migrations/2021_01_16_204635_initial_database.php
+++ database/migrations/2021_01_16_204635_initial_database.php
@@ -27,6 +27,54 @@ class InitialDatabase extends Migration
             $table->foreignUuid('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
+
+        Schema::create('units', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->foreignUuid('course_id')->references('id')->on('courses');
+            $table->timestamps();
+        });
+
+        Schema::create('videos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('url');
+            $table->foreignUuid('unit_id')->references('id')->on('units');
+            $table->timestamps();
+        });
+
+        Schema::create('files', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('path');
+            $table->foreignUuid('unit_id')->references('id')->on('units')->nullable(true);
+            $table->foreignUuid('course_id')->references('id')->on('courses');
+            $table->timestamps();
+        });
+
+        Schema::create('activities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->enum('status', ['inactive', 'active'])->default('inactive');
+            $table->string('title');
+            $table->foreignUuid('unit_id')->references('id')->on('units')->nullable(true);
+            $table->foreignUuid('course_id')->references('id')->on('courses');
+            $table->timestamps();
+        });
+
+        Schema::create('questions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('description');
+            $table->foreignUuid('activity_id')->references('id')->on('activities');
+            $table->timestamps();
+        });
+
+        Schema::create('options', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('description');
+            $table->boolean('correct')->default(false);
+            $table->foreignUuid('question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -36,6 +84,12 @@ class InitialDatabase extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('options');
+        Schema::dropIfExists('questions');
+        Schema::dropIfExists('activities');
+        Schema::dropIfExists('files');
+        Schema::dropIfExists('videos');
+        Schema::dropIfExists('units');
         Schema::dropIfExists('courses');
         Schema::dropIfExists('categories');
     }
