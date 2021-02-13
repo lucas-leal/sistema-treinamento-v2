aac6c9a750d9e7fe810186981350aeaec1365462
Lucas Leal <lucas.ity.leal@gmail.com>
2021-02-13 17:05:18 -0300

    Aadjusting users layout.

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,51 +1,55 @@
 @extends('layout')
 
 @section('main')
-    <form action="/users" method="post">
-        @csrf
-        <h3>Register user</h3>
-        <div class="form-row col-md-8">
-            <div class="form-group col-md-6">
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror">
-                <div class="invalid-feedback">
-                    @error('name')
-                        {{ $message }}
-                    @enderror
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-md-8">
+            <h4 class="mb-3">Register user</h4>
+            <form action="/users" method="post">
+                @csrf
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror">
+                        <div class="invalid-feedback">
+                            @error('name')
+                                {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
 
-            <div class="form-group col-md-6">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror">
-                <div class="invalid-feedback">
-                    @error('email')
-                        {{ $message }}
-                    @enderror
+                    <div class="form-group col-md-6">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror">
+                        <div class="invalid-feedback">
+                            @error('email')
+                                {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="form-row col-md-8">
-            <div class="form-group col-md-6">
-                <label for="login">Login</label>
-                <input type="text" name="login" id="login" value="{{ old('login') }}" class="form-control @error('login') is-invalid @enderror">
-                <div class="invalid-feedback">
-                    @error('login')
-                        {{ $message }}
-                    @enderror
-                </div>
-            </div>
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label for="login">Login</label>
+                        <input type="text" name="login" id="login" value="{{ old('login') }}" class="form-control @error('login') is-invalid @enderror">
+                        <div class="invalid-feedback">
+                            @error('login')
+                                {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
 
-            <div class="form-group col-md-6">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror">
-                <div class="invalid-feedback">
-                    @error('password')
-                        {{ $message }}
-                    @enderror
+                    <div class="form-group col-md-6">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror">
+                        <div class="invalid-feedback">
+                            @error('password')
+                                {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
         </div>
-        <button type="submit" class="btn btn-primary">Register</button>
-    </form>
+    </div>
 @endsection
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,23 +1,30 @@
 @extends('layout')
 
 @section('main')
-    <h1>Users</h1>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Login</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($users as $user)
-            <tr>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->login }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Users</h4>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Login</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->login }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
 @endsection
