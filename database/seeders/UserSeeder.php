da31bb14b2b5bfa1db3e9bf529da6256f5d073ec
Lucas Leal <lucas.ity.leal@gmail.com>
2021-02-17 08:57:55 -0300

    Seeding a common user.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,10 +18,20 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'id' => Uuid::uuid(),
             'name' => 'Administrator',
-            'email' => 'administrator@laravel.com',
+            'email' => 'administrator@administrator.com',
             'login' => 'admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'admin' => true,
+            'email_verified_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => Uuid::uuid(),
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'login' => 'user',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'admin' => false,
             'email_verified_at' => now(),
         ]);
     }
