0bb8d90c2aa0403a10b2a7fdcb14dc0a44354faa
Lucas Leal <lucas.ity.leal@gmail.com>
2021-02-20 17:50:23 -0300

    Added button "New" on listing courses and users.

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -1,29 +1,36 @@
 @extends('layout')
 
 @section('main')
-    <h1>Courses listing</h1>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Instructor</th>
-                <th>Keywords</th>
-                <th>View</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($courses as $course)
-                <tr>
-                    <td>{{ $course->id }}</td>
-                    <td>{{ $course->title }}</td>
-                    <td>{{ $course->category->name }}</td>
-                    <td>{{ $course->instructor }}</td>
-                    <td>{{ $course->keywords }}</td>
-                    <td><a href="{{ route('courses.view', ['id' => $course->id]) }}">View</a></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="mb-3">
+                <h3 class="d-inline">Courses</h3>
+                <a href="/courses/create" class="btn btn-primary float-right">New</a>
+            </div>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Instructor</th>
+                        <th>Keywords</th>
+                        <th>View</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($courses as $course)
+                        <tr>
+                            <td>{{ $course->id }}</td>
+                            <td>{{ $course->title }}</td>
+                            <td>{{ $course->category->name }}</td>
+                            <td>{{ $course->instructor }}</td>
+                            <td>{{ $course->keywords }}</td>
+                            <td><a href="{{ route('courses.view', ['id' => $course->id]) }}">View</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,7 +3,10 @@
 @section('main')
     <div class="row">
         <div class="col-md-8">
-            <h4>Users</h4>
+            <div class="mb-3">
+                <h3 class="d-inline">Users</h3>
+                <a href="/users/create" class="btn btn-primary float-right">New</a>
+            </div>
             <table class="table table-hover">
                 <thead>
                     <tr>
