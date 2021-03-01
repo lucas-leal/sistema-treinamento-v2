9890c0cfde45dc80b0908314abc6df1984a1cb2f
Lucas Leal <lucas.ity.leal@gmail.com>
2021-03-01 08:09:24 -0300

    Listing only curses which the users did not have subscribed.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $courses = Course::where([])->get();
+        $courses = Course::whereDoesntHave('users', function (Builder $query) {
+            $query->where('users.id', Auth::id());
+        })->get();
 
         return view('welcome', ['courses' => $courses]);
     }
